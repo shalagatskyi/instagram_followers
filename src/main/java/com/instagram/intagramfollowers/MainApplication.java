db6064c8ddf68590abb038edc61fbe33b39120bd
Shalagatskyi <shalagatskyi@gmail.com>
2018-03-06 02:03:18 +0200

    rename runner

--- src/main/java/com/instagram/intagramfollowers/MainApplication.java
+++ src/main/java/com/instagram/intagramfollowers/MainApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ParserApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ParserApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 }
